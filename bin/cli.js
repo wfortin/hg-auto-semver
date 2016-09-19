b7507361e484d28bc672f51bff8d3f57685ec1f5
William Fortin <wfortin@coveo.com>
2016-09-19 17:10:24 -0400

    Script to bump automagically (つ◕౪◕)つ━☆ﾟ.*･｡ﾟ

---
+++ bin/cli.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+'use strict';
+
+const exec = require('child_process').exec;
+const npm = require('npm');
+
+const Version = {
+    PATCH: 'patch',
+    MINOR: 'minor',
+    MAJOR: 'major'
+};
+
+function getParentBranch(callback) {
+    exec('hg log --rev "p2(.)" --template "{branch}"', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+
+        callback(stdout);
+    });
+}
+
+function bump(type) {
+    npm.load((error, npm) => {
+        console.log(error);
+        npm.commands.version([type], function(error) {
+            if (error) {
+                console.error(`npm version error: ${error}`);
+                return;
+            }
+        });
+    });
+}
+
+getParentBranch((branch) => {
+    console.trace()
+    const fixRegex = new RegExp('^fix\\-|[^a-z]fix\\-', 'gim');
+    const featureRegex = new RegExp('^feature\\-|[^a-z]feature\\-', 'gim');
+
+    if (fixRegex.test(branch)) {
+        console.log('Branch name contains "fix-", bumping a PATCH version');
+        bump(Version.PATCH)
+    } else if (featureRegex.test(branch)) {
+        console.log('Branch name contains "feature-", bumping a MINOR version');
+        bump(Version.MINOR)
+    }
+});
