0e5af9f63b689e7d7279a5f8aff84611e44a338f
William Fortin <wfortin@coveo.com>
2016-09-21 12:24:47 -0400

    Add commit, tag and publish to npm

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
-const exec = require('child_process').exec;
-const npm = require('npm');
+const execSync = require('child_process').execSync;
 
 const Version = {
     PATCH: 'patch',
@@ -10,40 +9,49 @@ const Version = {
     MAJOR: 'major'
 };
 
-function getParentBranch(callback) {
-    exec('hg log --rev "p2(.)" --template "{branch}"', (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-
-        callback(stdout);
-    });
+function getParentBranch() {
+    return execSync('hg log --rev "p2(.)" --template "{branch}"');
 }
 
 function bump(type) {
-    npm.load((error, npm) => {
-        console.log(error);
-        npm.commands.version([type], function(error) {
-            if (error) {
-                console.error(`npm version error: ${error}`);
-                return;
-            }
-        });
-    });
+    execSync(`npm version ${type}`);
 }
 
-getParentBranch((branch) => {
+function publish() {
+    const version = require('./package.json').version;
+
+    execSync(`hg commit --config ui.username=jenkins@coveo.com -m 'Release v${version}'`);
+    execSync(`hg tag --config ui.username=jenkins@coveo.com ${PACKAGE_VERSION} -f`);
+
+    execSync(`echo 'strict-ssl=false' > .npmrc`);
+    execSync(`echo '//npm.corp.coveo.com/:_authToken=${env.NPM_TOKEN}' >> .npmrc`);
+    execSync(`echo '@coveo:registry=https://npm.corp.coveo.com/' >> .npmrc`);
+
+    execSync(`npm publish`);
+
+    execSync(`hg --config auth.jenkins.prefix=* --config auth.jenkins.username=${process.env.HG_USR} --config auth.jenkins.password=${process.env.HG_PWD} --config 'auth.jenkins.schemes=http https' push`);
+}
+
+try {
+    const branch = getParentBranch();
     if (branch) {
         const fixRegex = new RegExp('^fix\\-|[^a-z]fix\\-', 'gim');
         const featureRegex = new RegExp('^feature\\-|[^a-z]feature\\-', 'gim');
 
         if (fixRegex.test(branch)) {
             console.log('Branch name contains "fix-", bumping a PATCH version');
-            bump(Version.PATCH)
+            bump(Version.PATCH);
+            publish();
         } else if (featureRegex.test(branch)) {
             console.log('Branch name contains "feature-", bumping a MINOR version');
-            bump(Version.MINOR)
+            bump(Version.MINOR);
+            publish();
         }
+    } else {
+        console.log('No parent branch, exiting');
     }
-});
+} catch (err) {
+    // We don't want our CI to have an error, just log it.
+    console.log(err);
+}
+
